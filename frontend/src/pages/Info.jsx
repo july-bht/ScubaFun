1debf7eb680bcccd4440a0b6bc4e11a3951162dd
Heggernn <98133382+Heggernn@users.noreply.github.com>
2022-08-09 12:43:25 +0200

    tdh

--- frontend/src/pages/Info.jsx
+++ frontend/src/pages/Info.jsx
@@ -1,123 +1,102 @@
-import React from 'react'
 import Line from "../components/Line";
+import React, { useState } from 'react';
+import data from '../assets/data.json';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
 
+const AccordionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  background: #fff;
+`;
 
-  const open = () => {
-    if (document.querySelector("#accordion").style.display = "block"){
-        document.querySelector("#accordion").style.display="none";
-    } else {
-      document.querySelector("#accordion").style.display="block";
+const Container = styled.div`
+  position: absolute;
+  top: 30%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
 
-    }
+const Wrap = styled.div`
+  background: #272727;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+  h1 {
+    padding: 2rem;
+    font-size: 2rem;
   }
+  span {
+    margin-right: 1.5rem;
+  }
+`;
 
+const Dropdown = styled.div`
+  background: #1c1c1c;
+  color: #00ffb9;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #00ffb9;
+  border-top: 1px solid #00ffb9;
+  p {
+    font-size: 2rem;
+  }
+`;
 
 const Info = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = index => {
+    if (clicked === index) {
+      //if clicked question is already active, then close it
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
   return (
-    <section className='sm:pt-8 sm:justify-center'>
-
+    <section className="pt-8">
     <div className='sm:text-center'>
-      <h2 className='sm:text-3xl sm:font-bold'>Praktisk Informasjon</h2>
-      < Line />
-    </div>
+    <h2 className='sm:text-3xl sm:font-bold'>Praktisk Informasjon</h2>
+    < Line />
+  </div>
+    <IconContext.Provider value={{ color: '#00FFB9', size: '25px' }}>
+      <AccordionSection>
+        <Container>
+          {data.info.map((item, index) => {
+            return (
+              <section className="grid md:grid-cols-2">
+              <div className="pb-5">
+                <Wrap onClick={() => toggle(index)} key={index}>
+                  <p className="text-2xl text-white">{item.title}</p>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+                </Wrap>
+                {clicked === index ? (
+                  <Dropdown>
+                    <h4 className="text-white">{item.text}</h4>
+                  </Dropdown>
+                ) : null}
+              </div>
+              </section>
+            );
+          })}
+        </Container>
+      </AccordionSection>
+    </IconContext.Provider></section>
+  );
+};
 
-    <section className='sm:flex sm:justify-center'>
-    <section className='sm:flex sm:w-3/5 sm:bg-secondary'>
-      {/* Left side */}
-      <div className='sm:p-8 sm:pl-8'>
-        <div className='mb-6'>
-          <div className='flex'>
-          <p className='font-bold text-black'>Hvem kan deltage</p>
-          <button onClick={open} id='block sm:hidden'>^</button>
-          </div>
-          <p id='accordion' className='text-black hidden text-sm sm:block'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Hvad skal jeg have med?</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Ansvar</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Dykkerstedet</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p> text-black
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Dykkerguiden</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-      </div>
-      {/* Left side END */}
-
-      {/* Right side */}
-      <div className='sm:p-8'>
-      <div className='mb-6'>
-          <p className='font-bold text-black'>Tilmelding</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Udstyr</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Udjelning af udstyr</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Betaling</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-
-        <div className='mb-6'>
-          <p className='font-bold text-black'>Gril</p>
-          <p className='text-black'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellendus obcaecati ratione at, id excepturi quibusdam eum saepe! Culpa libero veniam nisi labore consequatur voluptatum laudantium facere at sapiente repellat.
-            Totam libero esse in maiores mollitia, molestias, facere amet voluptatibus consectetur, corporis vitae perspiciatis. Neque adipisci deleniti assumenda architecto nisi, cum nam ducimus provident aperiam, ipsam repellendus! Consequatur, doloribus! Molestiae.
-          </p>
-        </div>
-      </div>
-      {/* Right side END*/}
-
-    </section></section>
-
-    </section>
-  )
-}
-
-export default Info
+export default Info;
