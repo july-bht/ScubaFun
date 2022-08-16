544241b79ea4b28b7d21c76ea0b6aabe6139c8bf
july-bht <83638794+july-bht@users.noreply.github.com>
2022-08-16 09:38:48 +0200

    json

--- frontend/src/pages/components/TripCard.jsx
+++ frontend/src/pages/components/TripCard.jsx
@@ -2,6 +2,15 @@
 import React from "react";
 import data from "../../assets/data.json";
 
+// {data.trips.map((i, key) => (
+
+
+// )))}
+
+
+
+  
+
 const Card = () => {
   const [showModal, setShowModal] = React.useState(false);
   return (
@@ -50,11 +59,11 @@ const Card = () => {
       {showModal ? (
         <>
           <div className="h-screen w-screen justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative  my-6 mx-auto max-w-3xl w-96 ">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black opacity-80 outline-none focus:outline-none">
                 {/*header*/}
-                {data.trips.map((i, key) => (
+                
                   <div>
                   <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl text-white font-semibold">
@@ -70,7 +79,7 @@ const Card = () => {
                   </button>
                 </div>
                 {/*body*/}
-                <img src="" alt="" />
+                <img className="w-full h-56 " src="" alt="" />
                 <div>
                     <h3 className="text-white">båd</h3>
                     <div></div>
@@ -86,7 +95,7 @@ const Card = () => {
                   <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b"></div>
                 </div>
                 </div>
-                ))}
+           
                 
               </div>
             </div>
