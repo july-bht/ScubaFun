25fe284fe9800248eb65bd25d7f631219508d64d
Heggernn <98133382+Heggernn@users.noreply.github.com>
2022-08-16 09:37:43 +0200

    sfods

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -3,12 +3,35 @@ import Line from '../components/Line'
 
 const SignUp = () => {
   return (
-    <section className='p-12'>
-      <h3 className="title"></h3>
+    <section className='p-12 pt-28'>
+      <h3 className="title">Tilmelding</h3>
       <Line/>
-      <div className='flex gap-5'>
-        <div className='bg-secondary'></div>
-        <div className='bg-secondary'></div>
+      <div className='flex gap-5 pt-5 px-32 flex-col md:flex-row'>
+
+        <div className="bg-secondary flex flex-col px-8 p-5 justify-between w-full text-center cursor-pointer">
+           <p className="p-2 pl-4 font-bold text-2xl text-black grid justify-center">Praktisk Information</p>
+
+          <div>
+            <div className='flex gap-2 justify-between'>
+              <div className='w-full'>
+              <p className='text-2xl text-black w-full text-left'>Sted</p>
+              </div>
+
+              <div className='w-full '>
+              <p className='text-2xl text-black w-full text-left pl-4 md:pl-20'>-Grenaa</p>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-secondary flex flex-col px-8 p-5 justify-between w-full text-center cursor-pointer">
+        <input className='p-1 bg-white text-black rounded' type="text" name="email" placeholder='Navn' />
+        </div>
+      </div>
+
+      <div className='flex justify-center pt-12'>
+      <button className='mr-4 h-8 transition duration-500'>Send Tilmedlign</button>
       </div>
     </section>
   )
