ef0634db4dae02d4263dd6d639401d8f4323b507
DDDASHXD <dddashxd@gmail.com>
2022-08-30 15:29:25 +0200

    Revert "fjerner ubrugte filer"
    This reverts commit 670b0b541544e3b51e1391570c6d5c384744a7ae.

---
+++ frontend/src/pages/components/PlacesAside.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from "../../components/Search";
+
+const PlacesAside = () => {
+  return (
+    <aside className='pt-10 absolute w-1/5 flex flex-col justify-start'>
+
+<Search/>
+
+    </aside>
+  )
+}
+
+export default PlacesAside
---
+++ frontend/src/pages/components/TripModal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <>
+      {/* make modal */}
+     
+    </>
+  );
+};
+
+export default Modal;
